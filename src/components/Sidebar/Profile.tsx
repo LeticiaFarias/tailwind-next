01e025a748977931ba47c30c0ca018ddc79aa288
leticiafarias <leticia_farias@alu.ufc.br>
2023-08-30 16:48:07 -0300

    feat: minor changes

--- src/components/Sidebar/Profile.tsx
+++ src/components/Sidebar/Profile.tsx
@@ -5,17 +5,13 @@ export function Profile() {
   return (
     <div className="grid grid-cols-profile items-center gap-3">
       <img
-        src="https://github.com/leticiafarias.png"
+        src="https://images.unsplash.com/photo-1618835962148-cf177563c6c0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzN8fHJvc3RvfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60"
         className="h-10 w-10 rounded-full"
         alt=""
       />
       <div className="flex flex-col truncate">
-        <span className="text-sm font-semibold text-zinc-700">
-          Letícia Farias
-        </span>
-        <span className="truncate text-sm text-zinc-500">
-          leticiafarias.dev@gmail.com
-        </span>
+        <span className="text-sm font-semibold text-zinc-700">Jane Doe</span>
+        <span className="truncate text-sm text-zinc-500">janedoe@mail.com</span>
       </div>
       <button type="button" className="ml-auto rounded-md p-2 hover:bg-zinc-50">
         <LogOut className="h-5 w-5 text-zinc-500" />
